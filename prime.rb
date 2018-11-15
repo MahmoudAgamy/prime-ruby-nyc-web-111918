5ca81700ad419842f49083ea374e7cdbe11c09d8
Mahmoud Agamy <agamyv@gmail.com>
2018-11-15 09:44:38 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,8 +1,11 @@
 def prime?(n)
-  for number in (2..(n/2)) do 
-    if n % number == 0 
-      return false
+  if n <= 1 
+    return false
+  else
+    for number in (2..(n/2)) do 
+      if n % number == 0 
+        return false
+      end
     end
-  end
   return true
 end
